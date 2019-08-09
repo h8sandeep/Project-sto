47018eb2306db410189ab4d61efb9f11461c6491
Paravada Pavan Sandeep <h8sandeep@gmail.com>
2019-08-09 19:56:15 +0530

    project stock
    project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StocklistComponent } from './stocklist/stocklist.component';
+import { StockbodyComponent } from './stockbody/stockbody.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StocklistComponent,
+    StockbodyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stockbody/stockbody.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stockbody',
+  templateUrl: './stockbody.component.html',
+  styleUrls: ['./stockbody.component.css']
+})
+export class StockbodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
